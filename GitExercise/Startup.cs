d6f17f447c5636dbd047a538f2cb3fec0635ee76
Zheni <jenijivkova07@gmail.com>
2025-10-22 09:20:04 +0300

    Division, Abs Substraction added; Introduced authorisation

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -4,8 +4,26 @@ namespace GitExercise
 {
     public class Startup
     {
+        private static bool CheckCredentials()
+        {
+            Console.WriteLine("Enter password to gain acess:");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+
+        private const string Password = "abcd1234";
+       
         public static void Main()
         {
+            bool isAuthorised = CheckCredentials();
+            if (!isAuthorised)
+            {
+                Console.WriteLine("Access denied.");
+                Console.ReadKey(true);
+                return;
+            }
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -34,6 +52,12 @@ namespace GitExercise
                     break;
                 case "m":
                     OptionsManager.Multiply(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a, b);
                     break;
             }
 
